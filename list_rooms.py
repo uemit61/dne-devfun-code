4cce93bc247cb0d7b0777f911f9d2c73c4df6b1c
uemit61 <hopes61@icloud.com>
2023-12-20 15:04:50 +0100

    Writing a Get-Response with Python

---
+++ list_rooms.py
@@ -0,0 +1,12 @@
+import requests
+
+apiUrl = 'https://webexapis.com/v1/rooms'
+access_token = "MTNkNzk4MWUtZWE3Zi00YzNhLWE3ZjktYzY3NmRkZjJjMGVhNDNkNWE1N2UtMjYy_P0A1_14a2639d-5e4d-48b4-9757-f4b8a23372de"
+
+httpHeaders = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token }
+queryParams = { 'sortBy': 'lastactivity', 'max': '2' }
+
+response = requests.get( url = apiUrl, headers = httpHeaders, params = queryParams )
+
+print( response.status_code )
+print( response.text )
